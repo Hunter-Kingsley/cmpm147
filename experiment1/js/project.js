25693da4193afea852970ab22e7888f62fb9c7a0
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-04-06 16:23:58 -0700

    Added in functionality for experiment 1

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,53 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Hunter Kingsley
+// Date: 04/06/2025
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
+const fillers = {
+  thirdPerson: ["bro", "dude", "my man", "homie", "girl", "queen"],
+  place: ["West City", "The Arctic", "Planet Namek", "Beerus's Planet", "The world of the Kais", "The Otherworld", "The Demon Realm", "Universe 6", "Universe 11", "Planet Cereal"],
+  enemy: ["Frieza", "Cell", "Kid Buu", "Broly", "Cooler", "Bojack", "Garlic Jr.", "Janemba", "Lord Slug", "Turles", "Metal Cooler", "Goku Black", "Zamasu", "Gas", "Moro", "Nappa"],
+  dragonBalls: ["the dragon balls", "the namekian dragon balls", "the super dragon balls", "the demon world dragon balls", "the cerealian dragon balls"],
+  CharA: ["Master Roshi", "Whis", "King Kai", "Kami", "Korin", "Beerus", "Merus", "Mr Popo"],
+  CharB: ["Goku", "Gohan", "Goten", "Vegeta", "Trunks", "Future Trunks", "Piccolo", "Pan", "Krillin", "Tien", "Yamcha", "Chiaotzu"],
+  technique: ["the Afterimage Technique", "the Kamehameha", "the Galick Gun", "the Final Flash", "the Destructo Disc", "Instant Transmission", "the Kaio-ken", "the Masenko", "the Spirit Bomb", "the Special Beam Cannon", "Time-Skip", "Dragon Fist", "the Evil Containment Wave"],
+  modifier: ["Super", "Ultra", "Evil", "Golden", "Super Saiyan God Super", "Great", "Orange", "Final", "Ultimate", "Black", "Full Power", "Legendary Super"],
+  transformation: ["Saiyan", "Instinct", "Ego", "Kaio-ken"],
+  number: ["1", "2", "3", "4", "5"],
+  adverb: ["really", "kinda", "absolute", "total", "a little"],
+  verb: ["peak fiction", "ass", "mid", "meh", "peak"]
+};
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
+const template = `Ayo $thirdPerson, did you hear about what happened in the latest arc of the Dragon Ball Super Manga?
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
+The Z-Fighters have to travel to $place because $enemy was revived using $dragonBalls. $CharA teaches $CharB how to use $technique, and at the end $CharB unlocks $modifier $transformation $number to save the day.
+
+Honestly I thought it was $adverb $verb.`;
+
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  box.innerText = story;
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+clicker.onclick = generate;
+
+generate();
